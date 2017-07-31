704fe693b93f3d70ba963f00d81d87434bcbc962
SummerXia <1056073482@qq.com>
2017-07-31 20:51:01 +0800

    Update apiConfig.js

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,13 +1,6 @@
 var baseServiceUrl = '',
 	jsonServiceUrl = '';
-//PRODUCTION 为一个全局变量
-// if (PRODUCTION) {
-// 	baseServiceUrl = 'http://wkf.hx168.com.cn/hxkf/api/v1.0/',
-// 	jsonServiceUrl = 'http://wkf.hx168.com.cn/hxwt/servlet/json?';
-// } else { 
-	baseServiceUrl = 'http://wxtest.hx168.com.cn/hxkf/api/v1.0/',
-	jsonServiceUrl = 'http://wxtest.hx168.com.cn/hxwt/servlet/json?';
-// }
+
 const apis = {
 	'encryptKey': "9d1f9019b1ba46efa343b55f64f6dcc90932fe50c0b0c2df047ebe321f2fe7c4a34bc42a890712aa0018501f32068c6c404a4fe8e4379670170ca0fae1e7e6b103eda2d3733787c1ef2447a28b813e3c0dfb86f85f72759a9e7201c68910eb585eb887123e07918ffdd4981cc5661331a2be61b498803f551f7989a8ec22cbb3",
 	'getVerifiImg': baseServiceUrl + 'stream/randimg?randKey=123456&width=96&height=32',
